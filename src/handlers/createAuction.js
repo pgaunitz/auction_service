f6e727696596aa23bffd6cef9f6a60dfd51a0ea1
Philip Gaunitz <fluffen@pgaunitz.se>
2020-06-10 16:45:02 +0200

    changed function to arrow function and the return

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,14 @@
-async function createAuction(event, context) {
+// async function createAuction(event, context) {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+//   };
+// }
+
+const createAuction = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+    body: JSON.stringify({ event, context }),
   };
 }
 
