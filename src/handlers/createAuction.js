8eea06cfd39c0f38efcfd9b8e655f83ae07e42ed
Philip Gaunitz <fluffen@pgaunitz.se>
2020-06-11 10:55:38 +0200

    updating the create auction function

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -6,12 +6,19 @@
 // }
 
 const createAuction = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
   };
-}
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+};
 
 export const handler = createAuction;
-
-
