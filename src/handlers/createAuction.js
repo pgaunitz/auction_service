fb98e78dce33361869467ddba4cf3f3644301eb1
Philip Gaunitz <fluffen@pgaunitz.se>
2020-06-11 15:17:26 +0200

    install uuid, adding DynamoDB documentClient to Insert auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,19 +1,23 @@
-// async function createAuction(event, context) {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
-//   };
-// }
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const createAuction = async (event, context) => {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
   };
+
+  await dynamodb.put({
+    TableName: "AuctionsTable",
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
