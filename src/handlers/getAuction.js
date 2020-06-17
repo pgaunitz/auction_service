51c16cb005ca0d8506923ee5dfe2ab644877f8c9
Philip Gaunitz <fluffen@pgaunitz.se>
2020-06-17 12:42:03 +0200

    refactoring getAuction and adds validation of a bid

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -4,9 +4,8 @@ import commonMiddleware from "../lib/commonMiddleware";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const getAuction = async (event, context) => {
+export async function getAuctionById(id) {
   let auction;
-  const { id } = event.pathParameters;
 
   try {
     const result = await dynamodb
@@ -25,6 +24,12 @@ const getAuction = async (event, context) => {
   if (!auction) {
     throw new createError.NotFound(`Auction with ID "${id}" not found!`);
   }
+  return auction;
+}
+
+const getAuction = async (event, context) => {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
 
   return {
     statusCode: 200,
