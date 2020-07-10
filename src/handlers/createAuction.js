a9ea6223672e24438661e055ea36177481d673ed
Philip Gaunitz <fluffen@pgaunitz.se>
2020-07-10 12:23:39 +0200

    adds email to create auction object

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -9,6 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const createAuction = async (event, context) => {
   const { title } = event.body;
+  const { email } = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -22,6 +23,7 @@ const createAuction = async (event, context) => {
     highestBid: {
       amount: 0,
     },
+    seller: email,
   };
 
   if (!auction.title) {
