c69c3ecdbab6f85fefacd2dd168195191d1a4f6c
Philip Gaunitz <fluffen@pgaunitz.se>
2020-07-14 16:33:43 +0200

    adds uploadAuctionPicture function, function to uload images to s3 and function for set the url of image

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,27 +3,33 @@ import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this item.`);
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
+  let updatedAuction;
+
   try {
-    const uploadToS3Result = await uploadPictureToS3(
-      auction.id + ".jpg",
-      buffer
-    );
-    console.log(uploadToS3Result);
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
   } catch (error) {
-    console.error(error)
-    throw new createError.InternalServerError(error)
+    console.error(error);
+    throw new createError.InternalServerError(error);
   }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
